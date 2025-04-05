a308635df0b5060501b885ea3461b48320c05297
tuandongoc291196 <tuandnse62052@fpt.edu.vn>
2025-04-05 20:27:44 +0700

    fix reports

--- src/main/java/com/fu/skincare/repository/BillRepository.java
+++ src/main/java/com/fu/skincare/repository/BillRepository.java
@@ -14,31 +14,26 @@ import com.fu.skincare.response.bill.BillStatusReport;
 
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Integer> {
-    Page<Bill> findAllByStatus(String status, Pageable pageable);
+        Page<Bill> findAllByStatus(String status, Pageable pageable);
 
-    Page<Bill> findAllByAccount(Account account, Pageable pageable);
+        Page<Bill> findAllByAccount(Account account, Pageable pageable);
 
-    @Query(value = "SELECT \n" + //
-            "    DATE(create_at) AS date, \n" + //
-            "    SUM(total_price) AS total_price\n" + //
-            "FROM \n" + //
-            "    bill\n" + //
-            "WHERE \n" + //
-            "   status not like 'CANCELED'\n" + //
-            "   AND status not like 'REJECTED'\n" + //
-            "GROUP BY \n" + //
-            "    DATE(create_at);", nativeQuery = true)
-    List<Object[]> getBillReport();
+        @Query(value = "SELECT DATE(create_at) AS date, SUM(total_price) AS total_price\n" + //
+                        "FROM bill\n" + //
+                        "WHERE status like 'SUCCESS'\n" + //
+                        "\tOR status like 'DONE'\n" + //
+                        "GROUP BY DATE(create_at);", nativeQuery = true)
+        List<Object[]> getBillReport();
 
-    @Query(value = "SELECT \n" + //
-            "    COALESCE(count(distinct id), 0) AS total,\n" + //
-            "    COALESCE(SUM(CASE WHEN status = 'PENDING' THEN 1 ELSE 0 END), 0) AS pending,\n" + //
-            "    COALESCE(SUM(CASE WHEN status = 'APPROVED' THEN 1 ELSE 0 END), 0) AS approved,\n" + //
-            "    COALESCE(SUM(CASE WHEN status = 'REJECTED' THEN 1 ELSE 0 END), 0) AS rejected,\n" + //
-            "    COALESCE(SUM(CASE WHEN status = 'CANCELED' THEN 1 ELSE 0 END), 0) AS canceled,\n" + //
-            "    COALESCE(SUM(CASE WHEN status = 'SUCCESS' THEN 1 ELSE 0 END), 0) AS success,\n" + //
-            "    COALESCE(SUM(CASE WHEN status = 'DONE' THEN 1 ELSE 0 END), 0) AS done\n" + //
-            "FROM \n" + //
-            "    bill;", nativeQuery = true)
-    BillStatusReport getBillStatusReport();
+        @Query(value = "SELECT \n" + //
+                        "    COALESCE(count(distinct id), 0) AS total,\n" + //
+                        "    COALESCE(SUM(CASE WHEN status = 'PENDING' THEN 1 ELSE 0 END), 0) AS pending,\n" + //
+                        "    COALESCE(SUM(CASE WHEN status = 'APPROVED' THEN 1 ELSE 0 END), 0) AS approved,\n" + //
+                        "    COALESCE(SUM(CASE WHEN status = 'REJECTED' THEN 1 ELSE 0 END), 0) AS rejected,\n" + //
+                        "    COALESCE(SUM(CASE WHEN status = 'CANCELED' THEN 1 ELSE 0 END), 0) AS canceled,\n" + //
+                        "    COALESCE(SUM(CASE WHEN status = 'SUCCESS' THEN 1 ELSE 0 END), 0) AS success,\n" + //
+                        "    COALESCE(SUM(CASE WHEN status = 'DONE' THEN 1 ELSE 0 END), 0) AS done\n" + //
+                        "FROM \n" + //
+                        "    bill;", nativeQuery = true)
+        BillStatusReport getBillStatusReport();
 }
--- src/main/java/com/fu/skincare/repository/OrderDetailRepository.java
+++ src/main/java/com/fu/skincare/repository/OrderDetailRepository.java
@@ -15,6 +15,7 @@ public interface OrderDetailRepository extends JpaRepository<OrderDetail, Intege
             "   from product p \n" + //
             "       join product_detail pd on pd.product_id  = p.id\n" + //
             "       join order_detail od on od.product_detail_id  = pd.id \n" + //
+            "   where od.status like 'SUCCESS' or od.status like 'DONE' \n" + //
             "   GROUP BY p.id, p.name \n" + //
             "   ORDER BY quantity DESC \n" + //
             "   LIMIT 5;", nativeQuery = true)
